aa9bd9bba526478165dabccc4945778bae432905
xian <pzhxbz@QQ.com>
2016-04-26 20:56:52 +0800

    Added files via upload

---
+++ include/Ellip.h
@@ -0,0 +1,26 @@
+#ifndef ELLIP_H
+#define ELLIP_H
+#include "ShapeInfo.h"
+
+class Ellip:public ShapeInfo
+{
+    public:
+        Ellip(int x1,int x2);
+        Ellip();
+        void print(int a,int x1,int y1,int x2,int y2);
+        void print();
+        void draw();
+        void draw_change();
+        int load(int a);
+        bool is_in(int x,int y);
+        void trans(int change_x,int change_y);
+        void add_change();
+    protected:
+    private:
+        int x1,y1;
+        int x2,y2;
+        int info;
+        int change_x1=0,change_y1=0;
+};
+
+#endif // ELLIP_H
